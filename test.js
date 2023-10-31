73f7ce20d294845bd0dbf287045f1cba83caece3
vietthang09 <vthcvn@gmail.com>
2023-10-31 18:57:20 +0700

    update

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
-const tiktok = "https://www.tiktok.com/@vangha1997/video/7274197436439088430";
+const tiktokURL = "https://www.tiktok.com/@vangha1997/video/7274197436439088430";
 const youtubeURL = "https://www.youtube.com/watch?v=GoXkMBWdV84";
-const instagram = "https://www.instagram.com/reel/CyLY1DsNmUh";
+const instagramURL = "https://www.instagram.com/reel/CyLY1DsNmUh";
 const facebookURL =
   "https://www.facebook.com/ngakakocak/videos/video-lucu-dan-gokil/607085686836013";
-  const fbDownloader = require("fb-downloader-scrapper")
+const tiktokDl = require("@sasmeee/tkdl");
 async function test() {
-  const data = await fbDownloader(facebookURL);
-  console.log(data); 
+  const data = await tiktokDl(tiktokURL);
+  console.log(data);
 }
 test();
